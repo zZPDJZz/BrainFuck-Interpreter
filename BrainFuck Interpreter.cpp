70aef590a9e04fcb3d745c4b31893a7da305dfa0
Peter J <peterjuliano1999@aol.com>
2025-01-16 01:02:31 -0500

    cleanup

--- BrainFuck Interpreter.cpp
+++ BrainFuck Interpreter.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <string>
 
-//make fuckin string
 const int fucklength = 7500;
 
 
 int main() {
+	//int pointers and arrays
 	std::cout << "Enter BrainFuck Code Here: \n";
 	int writepointer = 0;
 	int inputlength = 0;
@@ -17,66 +17,44 @@ int main() {
 	std::cout << "\n" << "\n";
 	for (int iteration = 0; iteration < inputlength; iteration++) {
 		char currentchar = fuckin[iteration];
-		
-
+		//run functions for brainfuck
 		switch (currentchar) {
 		case '+':
 			fuckout[writepointer]++;
-			
 			break;
 		case '-':
 			fuckout[writepointer]--;
-			
 			break;
 		case '<':
 			writepointer--;
-			
-
 			break;
 		case '>':
 			writepointer++;
-
 			break;
 		case '[':
 			if (fuckout[writepointer] != 0) {
 				temp = iteration;
 			}
-			
-
-
 			break;
 		case ']':
 			if (fuckout[writepointer] != 0) {
 				iteration = temp;
 			}
 			else {
-
 			}
-
-
 			break;
+			//print when called
 		case '.':
 			std::cout << static_cast<char>(fuckout[writepointer]);
-			
 			break;
 		case ',':
-			
-
-
 			break;
+			//blank = cycle iteration
 		default:
-
-
-
-
 			break;
 		}
-		
 	}
-
+	//clean
 	std::cout << "\n" << "\n";
-
 	return 0;
-
-	
 }
